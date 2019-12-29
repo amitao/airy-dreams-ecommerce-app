dbd5579a0583507f5d9a6bcc34dbe0059e0da58a
Amie Thao <amie.c.thao@gmail.com>
2019-12-29 15:06:25 -0600

    add shop component with data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.scss';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Homepage from '../../pages/Homepage/Homepage';
-
+import Shoppage from '../../pages/Shop/Shoppage';
 
 function App() {
-    return (
-        <div>
-            <Route exact path="/" component={Homepage} />
-        </div>
-    );
+  return (
+    <div>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/shop" component={Shoppage} />
+    </div>
+  );
 }
 
 export default App;
