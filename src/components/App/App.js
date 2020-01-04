87ae404c9fd482ed812feb1dedc169835aaed169
Amie Thao <amie.c.thao@gmail.com>
2020-01-03 19:19:58 -0600

    connect login to firebase

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,22 +6,41 @@ import Homepage from '../../pages/Homepage/Homepage';
 import Shoppage from '../../pages/Shop/Shoppage';
 import Navbar from '../../components/Nav/Nav';
 import LoginOut from '../../pages/Login-out/Login-out-state';
+import { auth } from '../../firebase/firebase.utils';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged( user => {
+      this.setState({ currentUser: user })
+      console.log(user);
+    })
+  }
 
 
-function App() {
-  return (
-    <div className="app-container">
 
-        <Navbar />
-
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/shop" component={Shoppage} />
-          <Route path="/log-in" component={LoginOut} />
-        </Switch>
-
-    </div>
-  );
+  render () {
+    return (
+      <div className="app-container">
+  
+          <Navbar />
+  
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/shop" component={Shoppage} />
+            <Route path="/log-in" component={LoginOut} />
+          </Switch>
+  
+      </div>
+    );
+  }
 }
 
 export default App;
