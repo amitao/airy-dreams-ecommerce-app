58ff65492610c9758293083e85d6e31f6721c059
Amie Thao <amie.c.thao@gmail.com>
2020-02-11 20:07:24 -0600

    use react-router hooks for onclick button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import './Header.scss';
 import CustomButton from '../Custom-button/Custom-button';
 
 
-const Header = (props) => {
+const Header = () => {
+  
+  let history = useHistory();
 
   return (
     <div className="header-container">
       <div className="headline">
         <h1>Be <span>glam</span> with Airy Dreams.</h1>
         <h3>shop now these styles</h3>
-        <CustomButton type="submit">Shop now</CustomButton>
+        <CustomButton type="submit" onClick={() => history.push("/shop")}>Shop now</CustomButton>
       </div>
     </div>
   );
